5b0cada41550d3e77a7cf31fac3bec15fdbb607c
Mani <manisaneforce@gmail.com>
2023-08-02 11:17:13 +0530

    Initial Commit

---
+++ Sales Order/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Sales Order
+//
+//  Created by San eforce on 02/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sales Order/Sales_OrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sales_OrderApp.swift
+//  Sales Order
+//
+//  Created by San eforce on 02/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Sales_OrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
