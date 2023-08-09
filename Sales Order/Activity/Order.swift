86fce7022e3da25ddd2beb5639b94a5d6ae40b54
Mani <manisaneforce@gmail.com>
2023-08-09 11:57:23 +0530

    add Stepper

--- Sales Order/Activity/Order.swift
+++ Sales Order/Activity/Order.swift
@@ -8,11 +8,10 @@
 import SwiftUI
 
 struct Order: View {
-    
     var body: some View {
         NavigationView {
-            VStack {
-                ZStack {
+            VStack(spacing: 0) {
+                ZStack(alignment: .top) {
                     Rectangle()
                         .foregroundColor(Color.blue)
                         .frame(height: 100)
@@ -27,110 +26,109 @@ struct Order: View {
                         
                         Text("Order")
                             .font(.system(size: 25))
-                            .font(.headline)
                             .fontWeight(.bold)
                             .foregroundColor(.white)
                             .padding(.top, 50)
                             .offset(x: -20)
                     }
                 }
-                .frame(maxWidth: .infinity)
-                .padding(.top, -420)
+                .cornerRadius(5)
                 .edgesIgnoringSafeArea(.top)
-                
-                VStack{
-                    
+                .frame(maxWidth: .infinity)
+                .padding(.top, -(UIApplication.shared.windows.first?.safeAreaInsets.top ?? 0 )) // Adjust for safe area
+                //.offset(y:-30)
+                VStack(alignment: .leading, spacing: 10) {
                     Text("DR. INGOLE")
                         .font(.system(size: 15))
-                    //.padding(.top, -515)
-                        .padding(.leading, -185)
-                        .multilineTextAlignment(.leading)
-                    HStack{
+                    HStack {
                         Image("SubmittedCalls")
                             .resizable()
-                            .frame(width: 20,height: 20)
+                            .frame(width: 20, height: 20)
                             .background(Color.blue)
                             .cornerRadius(10)
-                        //.renderingMode(.template)
-                        //.foregroundColor(.white)
-                            .padding(.leading, -180)
                         Text("9923125671")
                             .font(.system(size: 15))
-                        //.padding(.top, -515)
-                            .padding(.leading, -165)
-                            .multilineTextAlignment(.leading)
                     }
                     Text("Shivaji Park, Dadar")
                         .font(.system(size: 15))
-                    //.padding(.top, -515)
-                        .padding(.leading, -185)
-                        .multilineTextAlignment(.leading)
-                    
                     Text("ReliVet")
                         .font(.system(size: 15))
-                    //.padding(.top, -515)
-                        .padding(.leading, -185)
-                        .frame(alignment: .leading)
                         .foregroundColor(Color(#colorLiteral(red: 0.2901960784, green: 0.2901960784, blue: 0.2901960784, alpha: 1)))
-                    
                     
                     ScrollView(.horizontal, showsIndicators: true) {
                         HStack {
                             Text("Ectoparasiticidal")
                                 .padding()
-                            
                             Text("Dewormer")
                             Text("Health Supplement")
                             Text("Anti infective")
                             Text("Ectoparasiticidal")
-                            
                         }
                         .padding(.horizontal, 20)
                     }
                 }
-                .offset(y:-320)
+                .padding(.bottom, 20) // Adjust spacing
                 
-                Spacer()
-                VStack{
-                HStack{
-                    Image("logo_new")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 100,height: 70)
-                        .cornerRadius(4)
-                    VStack(alignment: .leading,spacing: 5){
-                        Text("FIPOREL_ S DOG 0.67 ML")
-                            .fontWeight(.semibold)
-                            .lineLimit(2)
-                            .minimumScaleFactor(0.5)
-                        Text("RLVT001")
-                            .font(.subheadline)
-                            .foregroundColor(.secondary)
-                        HStack{
-                            Text("MRP ₹0")
-                            
-                            Text("Price₹ 197.00")
-                            
-                            
-                        }
+                List(0 ..< 5) { item in
+                    HStack {
+                        Image("logo_new")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 70)
+                            .cornerRadius(4)
                         
+                        VStack(alignment: .leading, spacing: 5) {
+                            Text("FIPOREL_ S DOG 0.67 ML")
+                                .fontWeight(.semibold)
+                                .lineLimit(2)
+                                .minimumScaleFactor(0.5)
+                            Text("RLVT001")
+                                .font(.subheadline)
+                                .foregroundColor(.secondary)
+                            HStack {
+                                Text("MRP ₹0")
+                                Spacer()
+                                Text("Price₹ 197.00")
+                            }
+                            HStack {
+                                Text("Pipette")
+                                Spacer()
+                                HStack {
+                                    Text("Order Qty :")
+                                    Stepper("",
+                                            onIncrement: {
+                                                // Increment action
+                                            },
+                                            onDecrement: {
+                                                // Decrement action
+                                            })
+                                }
+                            }
+                            HStack{
+                                Text("Free : 0")
+                                Spacer()
+                                Text("₹0.00")
+                            }
+                            Divider()
+                            HStack{
+                                Text("tatal Qty: 0")
+                                Spacer()
+                                Text("₹0.00")
+                            }
+                        }
                     }
+                    .padding(.vertical, 5) // Adjust spacing
                 }
+                .listStyle(PlainListStyle()) // Add list style
             }
-                .offset(y:-480)
-            }
-            .padding(.top, 400)
-            .edgesIgnoringSafeArea(.top)
+            .padding(.top, 10) // Adjust top padding
+            .navigationBarHidden(true)
         }
-        .navigationBarHidden(true)
     }
 }
-
 
 struct Order_Previews: PreviewProvider {
     static var previews: some View {
         Order()
     }
 }
-
-
