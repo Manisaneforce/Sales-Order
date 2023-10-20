8fb65c26919b58d102cae91c6e234b1e4ecf6140
Mani <manisaneforce@gmail.com>
2023-10-20 16:23:40 +0530

    Error

--- Sales Order/ContentView.swift
+++ Sales Order/ContentView.swift
@@ -41,170 +41,173 @@ struct ContentView: View {
     var body: some View {
        
         NavigationView {
+            GeometryReader { geometry in
             ZStack{
                 ScrollView(showsIndicators: false){
-                VStack{
-                    
-                    Image("logo_new")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 250, height: 100)
-                    
-                    VStack{
-                        Text("Welcome to ReliVet")
-                            .font(.title)
-                            .bold()
-                            .font(.system(size: 24))
-                            .foregroundColor(Color.gray)
-                        
-                        
-                        Text("Sign in to continue")
-                            .font(.system(size: 20))
-                            .font(.title)
-                            .foregroundColor(Color.gray)
-                    }
-                    .padding(.top,40)
-                    
-                    
-                    LottieUIView(filename: "mobile_number").frame(width: 150,height: 150)
-                        .onAppear {
-                            print("Saved Value  \(userEmail)")
-                            if userEmail.isEmpty{
-                                //HomePageNvigater = false
-                            }else{
-                                if let window = UIApplication.shared.windows.first {
-                                    window.rootViewController = UIHostingController(rootView: HomePage())
-                                }
+                   
+                        VStack(alignment: .center){
+                            
+                            Image("logo_new")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 250, height: 100)
+                            VStack{
+                                Text("Welcome to ReliVet")
+                                    .font(.title)
+                                    .bold()
+                                    .font(.system(size: 24))
+                                    .foregroundColor(Color.gray)
+                                
+                                
+                                Text("Sign in to continue")
+                                    .font(.system(size: 20))
+                                    .font(.title)
+                                    .foregroundColor(Color.gray)
                             }
+                            .padding(.top,30)
                             
                             
-                        }
-                    
-                    Text("Enter your registered mobile number")
-                        .font(.system(size: 17))
-                        .font(.title)
-                        .foregroundColor(Color.gray)
-                        .padding(.bottom,30)
-                    
-                    
-                    TextField("Mobile Number", text: $phoneNumber)
-                    // .border(Color.blue, width: 2)
-                        .cornerRadius(5)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                    
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 5)
-                                .stroke(ColorData.shared.HeaderColor, lineWidth: 1.5)
-                        )
-                        .padding(.horizontal,15)
-                        .keyboardType(.numberPad)
-                        .onChange(of: phoneNumber, perform: { newValue in
-                            // Limit the phone number to 10 characters
-                            if newValue.count > 10 {
-                                phoneNumber = String(newValue.prefix(10))
-                                
-                            }
-                            // Remove non-numeric characters
-                            phoneNumber = phoneNumber.filter { "0123456789".contains($0) }
-                            //UserDefaults.standard.set(phoneNumber, forKey: "savedPhoneNumber")
-                        })
-                    
-                    
-                    
-                    if #available(iOS 15.0, *) {
-                        ZStack{
-                            Rectangle()
-                                .foregroundColor(ColorData.shared.HeaderColor)
-                                .frame(height: 40)
-                                .cornerRadius(2)
-                            Button(action: {
-                                
-                                let axn = "send/sms"
-                                let apiKey = "\(axn)&mobile=\(phoneNumber)"
-                                phoneNumber2 = phoneNumber
-                                print(phoneNumber2)
-                                
-                                AF.request("https://rad.salesjump.in/server/Db_Retail_v100.php?axn=" + apiKey, method: .post, parameters: nil, encoding: URLEncoding(), headers: nil)
-                                    .validate(statusCode: 200 ..< 299)
-                                    .responseJSON { response in
-                                        switch response.result {
-                                        case .success(let value):
-                                            if let json = value as? [String: AnyObject] {
-                                                guard let prettyJsonData = try? JSONSerialization.data(withJSONObject: json, options: .prettyPrinted) else {
-                                                    print("Error: Cannot convert JSON object to Pretty JSON data")
-                                                    return
-                                                }
-                                                guard let prettyPrintedJson = String(data: prettyJsonData, encoding: .utf8) else {
-                                                    print("Error: Could print JSON in String")
-                                                    return
-                                                }
-                                                
-                                                print(prettyPrintedJson)
-                                                jsondata.data = json
-                                                
-                                                let mobileNumber = json["mobile"] as? String ?? ""
-                                                if mobileNumber.count < 10 {
-                                                    // If the mobile number is less than 10 characters
-                                                    print(json["result"] as? String ?? "")
-                                                    ShowTost = (json["result"] as? String ?? "")
-                                                    showToast = true
-                                                    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                                                        showToast = false
-                                                    }
-                                                } else {
-                                                    NavigteBoll = true
-                                                    // If the mobile number is 10 characters or more
-                                                    print(json["msg"] as? String ?? "")
-                                                    ShowTost = json["msg"] as? String ?? ""
-                                                    showToast = true
-                                                    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                                                        showToast = false
-                                                    }
-                                                    
-                                                }
-                                                
-                                                
-                                                
-                                            }
-                                        case .failure(let error):
-                                            print(error)
+                            LottieUIView(filename: "mobile_number").frame(width: 150,height: 150)
+                                .onAppear {
+                                    print("Saved Value  \(userEmail)")
+                                    if userEmail.isEmpty{
+                                        //HomePageNvigater = false
+                                    }else{
+                                        if let window = UIApplication.shared.windows.first {
+                                            window.rootViewController = UIHostingController(rootView: HomePage())
                                         }
                                     }
-                            }) {
-                                Text("SEND OTP")
-                                //.font(.title)
-                                    .fontWeight(.heavy)
-                                    .foregroundColor(.white)
-                                    .font(.system(size: 18))
-                                    .multilineTextAlignment(.center)
-                                //.frame(width: 325, height: 40)
-                                    .background(ColorData.shared.HeaderColor)
-                                    .cornerRadius(10)
+                                    
+                                    
+                                }
+                            
+                            Text("Enter your registered mobile number")
+                                .font(.system(size: 17))
+                                .font(.title)
+                                .foregroundColor(Color.gray)
+                                .padding(.bottom,30)
+                            
+                            
+                            TextField("Mobile Number", text: $phoneNumber)
+                            // .border(Color.blue, width: 2)
+                                .cornerRadius(5)
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
+                            
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 5)
+                                        .stroke(ColorData.shared.HeaderColor, lineWidth: 1.5)
+                                )
+                                .padding(.horizontal,15)
+                                .keyboardType(.numberPad)
+                                .onChange(of: phoneNumber, perform: { newValue in
+                                    // Limit the phone number to 10 characters
+                                    if newValue.count > 10 {
+                                        phoneNumber = String(newValue.prefix(10))
+                                        
+                                    }
+                                    // Remove non-numeric characters
+                                    phoneNumber = phoneNumber.filter { "0123456789".contains($0) }
+                                    //UserDefaults.standard.set(phoneNumber, forKey: "savedPhoneNumber")
+                                })
+                            
+                            
+                            
+                            if #available(iOS 15.0, *) {
+                                ZStack{
+                                    Rectangle()
+                                        .foregroundColor(ColorData.shared.HeaderColor)
+                                        .frame(height: 40)
+                                        .cornerRadius(2)
+                                    Button(action: {
+                                        
+                                        let axn = "send/sms"
+                                        let apiKey = "\(axn)&mobile=\(phoneNumber)"
+                                        phoneNumber2 = phoneNumber
+                                        print(phoneNumber2)
+                                        
+                                        AF.request("https://rad.salesjump.in/server/Db_Retail_v100.php?axn=" + apiKey, method: .post, parameters: nil, encoding: URLEncoding(), headers: nil)
+                                            .validate(statusCode: 200 ..< 299)
+                                            .responseJSON { response in
+                                                switch response.result {
+                                                case .success(let value):
+                                                    if let json = value as? [String: AnyObject] {
+                                                        guard let prettyJsonData = try? JSONSerialization.data(withJSONObject: json, options: .prettyPrinted) else {
+                                                            print("Error: Cannot convert JSON object to Pretty JSON data")
+                                                            return
+                                                        }
+                                                        guard let prettyPrintedJson = String(data: prettyJsonData, encoding: .utf8) else {
+                                                            print("Error: Could print JSON in String")
+                                                            return
+                                                        }
+                                                        
+                                                        print(prettyPrintedJson)
+                                                        jsondata.data = json
+                                                        
+                                                        let mobileNumber = json["mobile"] as? String ?? ""
+                                                        if mobileNumber.count < 10 {
+                                                            // If the mobile number is less than 10 characters
+                                                            print(json["result"] as? String ?? "")
+                                                            ShowTost = (json["result"] as? String ?? "")
+                                                            showToast = true
+                                                            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                                                                showToast = false
+                                                            }
+                                                        } else {
+                                                            NavigteBoll = true
+                                                            // If the mobile number is 10 characters or more
+                                                            print(json["msg"] as? String ?? "")
+                                                            ShowTost = json["msg"] as? String ?? ""
+                                                            showToast = true
+                                                            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                                                                showToast = false
+                                                            }
+                                                            
+                                                        }
+                                                        
+                                                        
+                                                        
+                                                    }
+                                                case .failure(let error):
+                                                    print(error)
+                                                }
+                                            }
+                                    }) {
+                                        Text("SEND OTP")
+                                        //.font(.title)
+                                            .fontWeight(.heavy)
+                                            .foregroundColor(.white)
+                                            .font(.system(size: 18))
+                                            .multilineTextAlignment(.center)
+                                        //.frame(width: 325, height: 40)
+                                            .background(ColorData.shared.HeaderColor)
+                                            .cornerRadius(10)
+                                    }
+                                }
+                                .padding(15)
+                                
+                                
                             }
-                        }
-                        .padding(15)
-                        
-                        
-                    }
-                    if NavigteBoll {
-                        if #available(iOS 15.0, *) {
-                            NavigationLink(
-                                destination: OTPVerify(numberOffFields: 6, jsondata: $jsondata), // Make sure you're using $jsondata here
-                                isActive: $NavigteBoll
-                            ) {
+                            if NavigteBoll {
+                                if #available(iOS 15.0, *) {
+                                    NavigationLink(
+                                        destination: OTPVerify(numberOffFields: 6, jsondata: $jsondata), // Make sure you're using $jsondata here
+                                        isActive: $NavigteBoll
+                                    ) {
+                                        EmptyView()
+                                    }
+                                } else {
+                                    // Handle non-iOS 15 case if needed
+                                }
+                            }
+                            NavigationLink(destination: HomePage(), isActive: $HomePageNvigater) {
                                 EmptyView()
                             }
-                        } else {
-                            // Handle non-iOS 15 case if needed
+                            
+                            Spacer()
                         }
-                    }
-                    NavigationLink(destination: HomePage(), isActive: $HomePageNvigater) {
-                        EmptyView()
-                    }
+                       
                     
-                    Spacer()
                 }
-            }
                 .ignoresSafeArea(.keyboard, edges: .bottom)
                 .onTapGesture {
                     UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
@@ -212,8 +215,10 @@ struct ContentView: View {
                 .onAppear {
                     UIScrollView.appearance().bounces = false
                 }
-        }
+            }
             .toast(isPresented: $showToast, message: "\(ShowTost)")
+            .frame(width: geometry.size.width, height: geometry.size.height)
+        }
            
     }
         .navigationBarHidden(true)
