01320727c17a5ca9763e70a2d793604916023cef
Manisaneforce <123452726+Manisaneforce@users.noreply.github.com>
2024-05-16 16:09:15 +0530

    fix error

---
+++ Sales Order/Activity/TEST.swift
@@ -0,0 +1,18 @@
+//
+//  TEST.swift
+//  Sales Order
+//
+//  Created by San eforce on 15/05/24.
+//
+
+import SwiftUI
+
+struct TEST: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TEST()
+}
