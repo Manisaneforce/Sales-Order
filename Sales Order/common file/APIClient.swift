436123ae80ded6fe1ff88313f3afbd93f97d0329
Mani <manisaneforce@gmail.com>
2024-07-01 16:39:08 +0530

    Change  Live

--- Sales Order/common file/APIClient.swift
+++ Sales Order/common file/APIClient.swift
@@ -9,9 +9,9 @@ import Foundation
 class APIClient{
     static let shared = APIClient()
     //BasURL
-    //var BaseURL: String = "https://rad.salesjump.in"
+    var BaseURL: String = "https://rad.salesjump.in"
     //Qa URL
-    var BaseURL: String = "http://qa.salesjump.in"
+    //var BaseURL: String = "http://qa.salesjump.in"
     var DBURL="/server/Db_Retail_v100.php?axn="
     var TestDBURL="/server/Db_Retail_v100-Mani.php?axn="
     var DB_native_Scheme = "/server/native_Db_V13.php?axn="
