3b90d1d515de69773c458b942ef86a3827cae747
Mani <manisaneforce@gmail.com>
2024-12-11 10:25:51 +0530

    Build Update 10/12/2024

---
+++ Sales Order/common file/XML TO Image .swift
@@ -0,0 +1,18 @@
+//
+//  XML TO Image .swift
+//  Sales Order
+//
+//  Created by Anbu j on 10/12/24.
+//
+
+import SwiftUI
+
+struct XML_TO_Image_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    XML_TO_Image_()
+}
