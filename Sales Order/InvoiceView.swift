b92388f9f759932c2f42b41d3951d615cd93971a
Mani <manisaneforce@gmail.com>
2025-01-10 11:55:59 +0530

    View invoice details

---
+++ Sales Order/InvoiceView.swift
@@ -0,0 +1,18 @@
+//
+//  InvoiceView.swift
+//  Sales Order
+//
+//  Created by Anbu j on 10/01/25.
+//
+
+import SwiftUI
+
+struct InvoiceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InvoiceView()
+}
